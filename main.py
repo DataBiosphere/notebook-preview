11b2168b35c18b934410852bd250da502b02aa3c
Brian Reilly <breilly@broadinstitute.org>
2019-06-20 13:25:04 -0400

    Add app.yaml and make it run in GAE standard

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from sanic import Sanic
 from sanic import response
+from sanic_cors import CORS, cross_origin
 from utils import perform_notebook_conversion, authorized
 
 
 # Webservice routing
 app = Sanic('douglass')
 app.config.from_pyfile('config.py')
+CORS(app)
 
 
 @app.get('/status')
 async def status(request):
     return response.text('OK')
 
-
-@app.post('/api/convert')
+@app.route('/api/convert', methods={'POST','OPTIONS'})
+@cross_origin(app, automatic_options=True)
 @authorized(app.config.SAM_ROOT)
 async def convert(request):
     return response.html(await perform_notebook_conversion(request.json))
